016a49f1dcfea5426c72dbb3bcdf798a3cd1406c
unknown <mohamedali.bouzaiene@esprit.tn>
2020-12-25 00:22:24 +0100

    added a meme generator

---
+++ commands/memes.js
@@ -0,0 +1,22 @@
+const fetch  = require("node-fetch")
+
+module.exports = {
+    
+    name: "meme",
+    description: "Generate a random meme.",
+    execute(message, args) {
+        let meme;
+        if (args[0] === '-h')
+            message.reply(this.description);
+        else if (args[0] !== "-h" && args != false)
+            message.reply("Wrong argument!")
+        else {
+            fetch("https://meme-api.herokuapp.com/gimme")
+                .then(res => { return res.json() })
+                .then(data => {
+                    message.channel.send(data.preview[data.preview.length-1]);
+
+                })
+        }
+    }
+};
